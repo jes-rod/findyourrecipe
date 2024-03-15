bf21d869f4ede65da1ed9da81ef638a2c026be5f
Jeser Rodriguez <104109970+jes-rod@users.noreply.github.com>
2024-03-15 15:39:23 -0600

    Update Main.js

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -7,20 +7,38 @@ import { searchName, searchIngredients } from '../../api/apiFunctions.js'
 
 function Main(props) {
   const [name, setName] = useState("");
-  const [ingredients, setIngredients] = useState("");
+  const [ingredients, setIngredients] = useState("meat tomato lettuce");
   const [vegan, setVegan] = useState(false);
   const [vegetarian, setVegetarian] = useState(false);
   const [gluten, setGluten] = useState(false);
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false)
+
+  function loadingFunction(){
+    if(!loading){
+      return <></>
+    }else if (loading === "loading"){
+      return  <div style={{display:"flex", justifyContent:"center", margin:"20px 0px"}}>
+                <img alt="Loading icon" src="/loading.gif" style={{height:"50px"}} ></img>
+              </div>
+    }else if(loading){
+      return <SearchResults setFlag={props.setFlag} results={results} selectRecipe={props.selectRecipe} />
+    }
+  }
   
   const searchByName = async (e) => {
     e.preventDefault();
+    setLoading("loading");
     const searchResults = await searchName(name, vegan, vegetarian, gluten);
     if(searchResults.code){
+      setLoading(false);
       alert(searchResults.message);
+      
     }
     if(searchResults.length === 0){
+      setLoading(false);
       alert("No recipes found");
+      
     }
     const processedResults = [];
     for (let i = 0 ; i < searchResults.length ; i++){
@@ -32,16 +50,22 @@ function Main(props) {
       }) 
     }
     setResults(processedResults);
+    setLoading(true);
   }
 
   const searchByIngredients = async (e) => {
     e.preventDefault();
+    setLoading("loading")
     const searchResults = await searchIngredients(ingredients);
     if(searchResults.code){
+      setLoading(false);
       alert(searchResults.message);
+      
     }
     if(searchResults.length === 0){
+      setLoading(false);
       alert("No recipes found");
+      
     }
 
     const processedResults = [];
@@ -54,6 +78,7 @@ function Main(props) {
       }) 
     }
     setResults(processedResults);
+    setLoading(true);
   }
 
   const changeVegan = () => {
@@ -77,10 +102,13 @@ function Main(props) {
                <SearchName searchByName={searchByName} setName={setName} changeVegan ={changeVegan} changeVegetarian={changeVegetarian} changeGluten={changeGluten}/>
                <SearchIngredients searchByIngredients={searchByIngredients} setIngredients={setIngredients}/>
             </div>
+            {
+
+            }
           </div>
         </div>
       </div>
-      <SearchResults setFlag={props.setFlag} results={results} selectRecipe={props.selectRecipe} />
+      {loadingFunction()}
     </div>
   );
 }
